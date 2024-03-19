d0a66e366a88e68c32b7b4693a6e40f09f2560db
s26850 <s26850@gmail.com>
2024-03-19 01:18:30 +0100

    Modification 1

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("I love my shoes!");
+}
