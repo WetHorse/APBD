4aebc982db61c30f1b484fad064cdb1fd2722876
s26850 <s26850@gmail.com>
2024-03-19 02:04:57 +0100

    Function taking an array and returns the max value.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -30,5 +30,11 @@ static float calculateTheAverage(int [] arr )
     return counter;
 }
 
-int[] arr = [1,2,3,4,5,6,7,8,9,10,11,14];
+static int calculateTheMax(int[] arr)
+{
+    return arr.Max();
+}
+
+int[] arr = [1,2,3,4,5,6,7,8,21,9,10,11,14];
 Console.WriteLine(calculateTheAverage(arr));
+Console.WriteLine(calculateTheMax(arr));
